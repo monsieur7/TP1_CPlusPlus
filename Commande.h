a4c53260192a4bd83a4b326b1ec93f3e8928cc0a
mawissocq <maxime.wissocq@etu.uca.fr>
2022-11-29 11:11:04 +0100

    Question 7

--- Commande.h
+++ Commande.h
@@ -1,4 +1,8 @@
 #include <vector>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+
 #include "Client.h"
 #include "Produit.h"
 enum class Status {
@@ -17,9 +21,30 @@ class Commande {
         Commande(int id, Client client);
         int getId();
         const Client& getClient();
+        std::vector<Produit> getListeProduit();
+        std::vector<Status> getListeStatus();
         void addProduit(Produit produit);
         void changeStatus(Status status, int it);
         int getNombreDeProduit();
+
+        friend std::ostream& operator << (std::ostream& os, Commande commande){
+        std::stringstream ss;
+        std::stringstream ss2;
+        auto liste_produit = commande.getListeProduit();
+        auto liste_status = commande.getListeStatus();
+        for (auto it = liste_produit.begin(); it != liste_produit.end(); it++){
+            ss << " "  << *it;
+        }
+        /*
+        for (auto it2 = liste_status.begin(); it2 != liste_status.end(); it2++){
+            ss2 << " "  << *it2;
+        }
+        */
+
+        os << "Id commande: " << commande.getId() << " client: " << commande.getClient() << 
+        " liste des produits: " << ss.str() /*<< " status du produit: " << ss2.str()*/;
+        return os;
+    }
         
 
 
